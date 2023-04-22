6c4b95ba48d6c5809cf12cc23c36f3d872c053ca
Evgeni <ewgeny11@mail.ru>
2023-04-22 22:15:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bil = 3250;     //стоимость билета
+        int bonus = 20;     //количество рублей за 1 бонус
+
+        int mil = bil / bonus;      //количество миль
+
+        System.out.println(mil);
+
+    }
+
+}
